957e945eef49fa47982c41b5d6354453c182e1b8
jakimiszka <jarek.jakimicha@gmail.com>
2020-10-04 19:59:37 +0200

    okrzyk mastera

--- Hello.java
+++ Hello.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 public class Hello{
 	
-	private String name;
-	private int age;
+	static String name = "Siemanko sportowe swiry!!!";
 
 	public static void main(String [] args){
 		System.out.println("Hello in Git world!!!");
 		System.out.println("Today date is: " + new Date());
+
+		System.out.println("Okrzyk mastera to: " + name);
 	}
 }
